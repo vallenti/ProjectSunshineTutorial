c1569f9e86714432f90736187e4b0e68f4d56044
Valio <vallentin.k@gmail.com>
2016-12-09 16:24:34 +0200

    added contract for the weather

--- app/src/main/java/bg/val/projectsunshine/app/DetailActivity.java
+++ app/src/main/java/bg/val/projectsunshine/app/DetailActivity.java
@@ -5,15 +5,54 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("DetailSAct", "ON Resume");
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("DetailSAct", "ON Destroy");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("DetailSAct", "ON Pause");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("DetailSAct", "ON Restart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("DetailSAct", "ON Start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("DetailSAct", "ON Stop");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("DetailSAct", "ON Create");
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
